49d915b3d3306140686d67ed1f9d2a967bddde38
srikanth <bommisettysrikanth88@gmail.com>
2025-04-11 10:23:31 +0530

    terraform vpc module

---
+++ outputs.tf
@@ -0,0 +1,16 @@
+# output "availability_zone_info" {
+#     value = data.aws_availability_zones.available
+  
+# }
+
+
+
+# output "aws_subnet_info" {
+#     value = aws_subnet.public
+  
+# }
+
+# output "aws_vpc_value" {
+#     value = aws_vpc.default
+  
+# }
