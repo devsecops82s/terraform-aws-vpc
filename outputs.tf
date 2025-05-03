8672a20c5d441908a083e5670a2eddaddcfe8aab
srikanth <bommisettysrikanth88@gmail.com>
2025-05-03 17:53:49 +0530

    terraform-module

--- outputs.tf
+++ outputs.tf
@@ -13,17 +13,17 @@ output "vpc_id" {
   
 }
 
-output "public_subnet_cidrs" {
+output "public_subnet_ids" {
     value = aws_subnet.public[*].id
   
 }
 
-output "private_subnet_cidrs" {
+output "private_subnet_ids" {
     value = aws_subnet.private[*].id
   
 }
 
-output "database_subnet_cidrs" {
+output "database_subnet_ids" {
     value = aws_subnet.database[*].id
   
 }
